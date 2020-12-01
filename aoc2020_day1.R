69b120c4647a9cbeb77994d9242e9dbac15f6cf1
Nabeel Alsalam <nabeel.alsalam@gmail.com>
2020-12-01 13:56:55 -0500

    Add comments for non-R users

--- aoc2020_day1.R
+++ aoc2020_day1.R
@@ -3,9 +3,12 @@
 
 v <- scan("input1a.txt")
 
+# outer sum
 x <- outer(v, t(v), FUN = "+")
 
-x[x == 2020] # there is a solution
+# Are there elements equal to 2020.  Yes two.
+x[x == 2020] 
+# What are the row, column indices of those?
 r <- which(x == 2020, arr.ind = TRUE)
 v[r[1,1]] + v[r[1,3]] # sum is 2020
 v[r[1,1]] * v[r[1,3]] # answer to part 1
@@ -13,6 +16,7 @@ v[r[1,1]] * v[r[1,3]] # answer to part 1
 # 1b
 # find product of three entries that sum to 2020
 
+# outer sum of out sum
 x <- outer(outer(v, t(v), FUN = "+"), t(v), FUN = "+")
 x[x == 2020] # there is a solution
 r <- which(x == 2020, arr.ind = TRUE)
