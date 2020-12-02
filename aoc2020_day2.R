5226e3905d3e37a00b8ac34b99532f13689601f6
Nabeel Alsalam <nabeel.alsalam@gmail.com>
2020-12-02 17:23:34 -0500

    add part 2 solution

--- aoc2020_day2.R
+++ aoc2020_day2.R
@@ -1,29 +1,6 @@
 # passwords that don't meet policy
 library(tidyverse)
 
-### Test
-
-input_test <-
-  tibble(
-    rep = c("1-3", "1-3", "2-9"),
-    char = c("a:", "b:", "c:"), 
-    passwd = c("abcde", "cdefg", "ccccccccc")
-  )
-
-passwds_test <-
-  input_test %>% 
-  mutate(char = substr(char, 1, 1)) %>% 
-  separate(rep, into = c("rep_min", "rep_max"), sep = "-")
-
-passwds_test %>%
-  mutate(
-    count = str_count(passwd, pattern = char), 
-    valid = count >= rep_min & count <= rep_max
-  ) %>%
-  count(valid)
-
-### 
-
 input <- 
   read_delim("input2.txt", 
              delim = " ", 
@@ -32,21 +9,38 @@ input <-
 passwds <-
   input %>% 
   mutate(char = substr(char, 1, 1)) %>% # strip the ":"
-  separate(rep, into = c("rep_min", "rep_max"), sep = "-") # split the policy
-         
+  separate(rep, into = c("rep_min", "rep_max"), sep = "-", remove = FALSE) %>%
+  mutate(across(.cols = c(rep_min, rep_max), .fns = as.numeric)) # failed to do this the 1st time
+
 # How many passwords are valid according to their policies?
 
-x <-
 passwds %>%
   mutate(
     count = str_count(passwd, pattern = char), 
-    valid = count >= rep_min & count <= rep_max
-  )
-View(x)
-  
-passwds %>%
-  mutate(
-    count = str_count(passwd, pattern = char), 
-    valid = count >= rep_min & count <= rep_max
+    valid = (count >= rep_min) & (count <= rep_max)
     ) %>%
-count(valid)  # This answer of 401 is too low 
+count(valid)
+
+# Part 2
+# New intpretation of the policy
+
+map_dfr(1:nrow(passwds), 
+# have to do a row at a time because the pattern parameter of str_detect cannot be a vector
+~ passwds[.x,] %>%
+  mutate(
+    valid = 
+      # exclusive or
+      xor(
+      str_detect(
+      passwd, 
+      # building the regular expression pattern from rep_min, rep_max, and char
+      # using multiple "." to anchor the position to look for the character
+      paste0("^", paste0(rep(".", rep_min - 1), collapse = ""), char, collapse = "")),
+    str_detect(
+      passwd, 
+      paste0("^", paste0(rep(".", rep_max - 1), collapse = ""), char, collapse = ""))
+    )
+  )
+) %>%
+count(valid)
+
