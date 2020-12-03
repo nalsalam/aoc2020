b8f1a5b36165a0c17f630d9d1e10a3addd57fb52
Jameel Alsalam <alsalam.jameel@epa.gov>
2020-12-03 14:53:08 -0500

    slight day 2 part 1 changes

--- aoc2020_day2.R
+++ aoc2020_day2.R
@@ -1,22 +1,24 @@
 # passwords that don't meet policy
 library(tidyverse)
 
-input <- 
-  read_delim("input2.txt", 
-             delim = " ", 
-             col_names = c("rep", "char", "passwd"))
+input <- read_lines("data-jra/input2.txt")
 
-passwds <-
-  input %>% 
-  mutate(char = substr(char, 1, 1)) %>% # strip the ":"
-  separate(rep, into = c("rep_min", "rep_max"), sep = "-", remove = FALSE) %>%
-  mutate(across(.cols = c(rep_min, rep_max), .fns = as.numeric)) # failed to do this the 1st time
+passwds <- tibble(
+  input = input
+) %>%
+  extract(
+    col = c(input),
+    regex = "(\\d+)-(\\d+) ([[:lower:]]): ([[:lower:]]+)",
+    into = c("rep_min", "rep_max", "char", "passwd"),
+    remove = FALSE
+  ) %>%
+  mutate(across(.cols = c(rep_min, rep_max), .fns = as.numeric))
 
 # How many passwords are valid according to their policies?
 
 passwds %>%
   mutate(
-    count = str_count(passwd, pattern = char), 
+    count = str_count(passwd, pattern = char),
     valid = (count >= rep_min) & (count <= rep_max)
     ) %>%
 count(valid)
@@ -24,21 +26,21 @@ count(valid)
 # Part 2
 # New intpretation of the policy
 
-map_dfr(1:nrow(passwds), 
+map_dfr(1:nrow(passwds),
 # have to do a row at a time because the pattern parameter of str_detect cannot be a vector
 ~ passwds[.x,] %>%
   mutate(
-    valid = 
+    valid =
       # exclusive or
       xor(
       str_detect(
-      passwd, 
+      passwd,
       # building the regular expression pattern from rep_min, rep_max, and char
       # using multiple "." to anchor the position to look for the character
       # paste0("^", paste0(rep(".", rep_min - 1), collapse = ""), char, collapse = "")),
       paste0("^.{", rep_min - 1, "}", char, collapse = "")),
     str_detect(
-      passwd, 
+      passwd,
       # paste0("^", paste0(rep(".", rep_max - 1), collapse = ""), char, collapse = ""))
       paste0("^.{", rep_max - 1, "}", char, collapse = ""))
     )
