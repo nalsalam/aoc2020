52a326fc1e708f072966e3e7aeadc34c7860ee1c
Jameel Alsalam <jalsalam@gmail.com>
2020-12-03 15:13:24 -0500

    improved day 2 part 2

--- aoc2020_day2.R
+++ aoc2020_day2.R
@@ -9,23 +9,39 @@ passwds <- tibble(
   extract(
     col = c(input),
     regex = "(\\d+)-(\\d+) ([[:lower:]]): ([[:lower:]]+)",
-    into = c("rep_min", "rep_max", "char", "passwd"),
+    into = c("num1", "num2", "char", "passwd"),
     remove = FALSE
   ) %>%
-  mutate(across(.cols = c(rep_min, rep_max), .fns = as.numeric))
+  mutate(across(.cols = c(num1, num2), .fns = as.numeric))
 
 # How many passwords are valid according to their policies?
 
+valid_passwd_old <- function(passwd, char, rep_min, rep_max) {
+  count <- str_count(passwd, pattern = char)
+  valid <- (count >= rep_min) & (count <= rep_max)
+  valid
+}
+
 passwds %>%
-  mutate(
-    count = str_count(passwd, pattern = char),
-    valid = (count >= rep_min) & (count <= rep_max)
-    ) %>%
-count(valid)
+  mutate(valid = valid_passwd_old(passwd, char, num1, num2)) %>%
+  count(valid)
+
 
 # Part 2
 # New intpretation of the policy
 
+# Jam's solution:
+valid_passwd_new <- function(passwd, char, pos1, pos2) {
+  match1 <- str_sub(passwd, start = pos1, end = pos1) == char
+  match2 <- str_sub(passwd, start = pos2, end = pos2) == char
+  xor(match1, match2)
+}
+
+passwds %>%
+  mutate(valid = valid_passwd_new(passwd, char, num1, num2)) %>%
+  count(valid)
+
+# Dad's solution:
 map_dfr(1:nrow(passwds),
 # have to do a row at a time because the pattern parameter of str_detect cannot be a vector
 ~ passwds[.x,] %>%
@@ -38,11 +54,11 @@ map_dfr(1:nrow(passwds),
       # building the regular expression pattern from rep_min, rep_max, and char
       # using multiple "." to anchor the position to look for the character
       # paste0("^", paste0(rep(".", rep_min - 1), collapse = ""), char, collapse = "")),
-      paste0("^.{", rep_min - 1, "}", char, collapse = "")),
+      paste0("^.{", num1 - 1, "}", char, collapse = "")),
     str_detect(
       passwd,
       # paste0("^", paste0(rep(".", rep_max - 1), collapse = ""), char, collapse = ""))
-      paste0("^.{", rep_max - 1, "}", char, collapse = ""))
+      paste0("^.{", num2 - 1, "}", char, collapse = ""))
     )
   )
 ) %>%
