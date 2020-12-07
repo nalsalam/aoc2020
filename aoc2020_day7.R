110e29dfa22e45c677a8a803e885f3bdf3158cf7
Nabeel Alsalam <nabeel.alsalam@gmail.com>
2020-12-07 08:34:17 -0500

    rules in data structure and attempt at part 2. Think recursion is needed.

--- aoc2020_day7.R
+++ aoc2020_day7.R
@@ -16,7 +16,8 @@ create_rules <- function(input) {
     str_replace(input, "^.+( bags contain )", "") %>%
     str_replace_all("bags|bag", "") %>%
     str_replace(" \\.", "") %>%
-    str_replace("no\\b", "0") %>%
+    # str_replace("no\\b", "0") %>% but "other" is not a color so ...
+    str_replace("no other", "") %>% # result is some outside colors have NA on inside vector
     str_split(" , ")
   
   inner_colors <-
@@ -32,23 +33,52 @@ create_rules <- function(input) {
 
 input <- read_lines("data-naa/input7_test.txt")
 rules <- create_rules(input)
-# NEED TO FIX: no other bags needs to be the empty vector
 
 # Count the ways 
 
-shiny_gold_inside <- function(outside_color) {
+shiny_gold_direct <- function(outside_color) {
   "shiny gold" %in% names(rules[[outside_color]])
 }
-map_lgl(outside_colors, shiny_gold_inside) %>% sum()  # directly
+map_lgl(outside_colors, shiny_gold_direct) %>% sum()  # directly
 
-# How about indirectly? Need to look for shiny gold one layer deeper
-# So need to go through inside colors of every rule also
+# How about eventually? Need to look for shiny gold one layer deeper
+# So go through inside colors of every rule also
+# Three layers??
 
-# This doesn't work.  Sorry.  Taking a break.
-shiny_gold_inside_2 <- function(outside_color, outside_colors = outside_colors) {
+shiny_gold_eventually <- function(outside_color) {
+  any(
+    "shiny gold" %in% names(rules[[outside_color]]),
   
-  if("shiny gold" %in% names(rules[[outside_color]])) TRUE
-  else map(outside_colors, shiny_gold_inside_2(.x))
-  else FALSE
+    {inside_colors <- names(rules[[outside_color]])
+    map_lgl(inside_colors, ~ "shiny gold" %in% names(rules[[.x]])) %>% any()}
+  )
 }
+
+shiny_gold_eventually("dark olive") 
+
+# shiny_gold_eventually is not vectorized
+testthat::expect_equal(
+  c(shiny_gold_eventually("bright white"), 
+    shiny_gold_eventually("muted yellow"), 
+    shiny_gold_eventually("dark orange"),
+    shiny_gold_eventually("light red"), 
+    shiny_gold_eventually("dark olive")), 
+  c(TRUE, TRUE, TRUE, TRUE, FALSE)
+)
+map_lgl(outside_colors, shiny_gold_eventually) %>% sum()
+
+# Part 1
+
+input <- read_lines("data-naa/input7.txt")
+
+outside_colors <-
+  str_extract(input, "^.+( bags contain)") %>%
+  str_replace(" bags contain", "")
+
+rules <- create_rules(input)
+
+map_lgl(outside_colors, shiny_gold_eventually) %>% sum()
+
+# Not the right answer -- probably need to go deep
+
 
